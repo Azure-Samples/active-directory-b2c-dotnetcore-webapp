8deece793a9c4f9d2bece2480f42050ed7e58123
Jenny Ferries <jeferrie@microsoft.com>
2019-08-02 17:44:27 -0700

    update to 4.3

--- WebApp-OpenIDConnect-DotNet/Models/StaticCache.cs
+++ WebApp-OpenIDConnect-DotNet/Models/StaticCache.cs
@@ -14,9 +14,8 @@ namespace WebApp_OpenIDConnect_DotNet.Models
         private static ReaderWriterLockSlim SessionLock = new ReaderWriterLockSlim(LockRecursionPolicy.NoRecursion);
         string UserId = string.Empty;
         string CacheId = string.Empty;
-        HttpContext httpContext = null;
-
-        ITokenCache cache;
+        private readonly HttpContext httpContext = null;
+        private ITokenCache cache;
 
         public MSALStaticCache(string userId, HttpContext httpcontext)
         {
@@ -31,27 +30,26 @@ namespace WebApp_OpenIDConnect_DotNet.Models
             this.cache = cache;
             cache.SetBeforeAccess(BeforeAccessNotification);
             cache.SetAfterAccess(AfterAccessNotification);
-            Load();
             return cache;
         }
 
-        public void Load()
+        public void Load(TokenCacheNotificationArgs args)
         {
             SessionLock.EnterReadLock();
             byte[] blob = staticCache.ContainsKey(CacheId) ? staticCache[CacheId] : null ;
             if(blob != null)
             {
-                cache.DeserializeMsalV3(blob);
+                args.TokenCache.DeserializeMsalV3(blob);
             }
             SessionLock.ExitReadLock();
         }
 
-        public void Persist()
+        public void Persist(TokenCacheNotificationArgs args)
         {
             SessionLock.EnterWriteLock();
 
             // Reflect changes in the persistent store
-            staticCache[CacheId] = cache.SerializeMsalV3();
+            staticCache[CacheId] = args.TokenCache.SerializeMsalV3();
             SessionLock.ExitWriteLock();
         }
 
@@ -59,7 +57,7 @@ namespace WebApp_OpenIDConnect_DotNet.Models
         // Reload the cache from the persistent store in case it changed since the last access.
         void BeforeAccessNotification(TokenCacheNotificationArgs args)
         {
-            Load();
+            Load(args);
         }
 
         // Triggered right after MSAL accessed the cache.
@@ -68,7 +66,7 @@ namespace WebApp_OpenIDConnect_DotNet.Models
             // if the access operation resulted in a cache update
             if (args.HasStateChanged)
             {
-                Persist();
+                Persist(args);
             }
         }
     }
